7dbd1b843632e71021758057a3b7d655db550820
SupiDupiToll <supidupi@magenta.de>
2025-09-19 22:26:40 +0200

    Update page.tsx

--- src/app/r/[slug]/page.tsx
+++ src/app/r/[slug]/page.tsx
@@ -16,9 +16,10 @@ function RickrollRedirect({ url }: { url: string }) {
   }, [url]);
   return (
     <div className="flex flex-col items-center justify-center min-h-[60vh] space-y-4">
-  <h2 className="text-2xl font-bold">Du wurdest gerickrollt!</h2>
+  <h2 className="text-2xl font-bold">Bitte warten...!</h2>
       <img src="https://media.giphy.com/media/Vuw9m5wXviFIQ/giphy.gif" alt="Rickroll GIF" className="rounded-lg shadow-lg w-80 h-60 object-cover" />
   <p className="text-neutral-500">Du wirst gleich weitergeleitet...</p>
+  <p className="text-neutral-500">...und du wurdest gerickrollt</p>
     </div>
   );
 }
